fcb0a59daca65522f59da3b1beaa0e310ae0627f
Daniel M. Ludwig <contact@daniel-ludwig.net>
2020-01-14 20:57:39 +0100

    Added base side drawer

--- app/src/main/java/de/byteinspace/mangaanimecollector/activities/BasicActivity.java
+++ app/src/main/java/de/byteinspace/mangaanimecollector/activities/BasicActivity.java
@@ -8,10 +8,11 @@ import android.content.Context;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.widget.RelativeLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -25,7 +26,7 @@ public abstract class BasicActivity extends AppCompatActivity {
    private Toolbar toolbar;
    private ListView drawerList;
    private ActionBarDrawerToggle drawerToogle;
-   protected RelativeLayout relativeLayout;
+   protected DrawerLayout drawerLayout;
    protected String activityTitle;
 
 
@@ -37,7 +38,7 @@ public abstract class BasicActivity extends AppCompatActivity {
 
         showToolbar();
         drawerList = (ListView)findViewById(R.id.navList);
-        relativeLayout = (RelativeLayout)findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         activityTitle = getTitle().toString();
 
         createDrawer(this);
@@ -56,52 +57,72 @@ public abstract class BasicActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        //setupDrawer();
+        setupDrawer();
 
     }
 
-//   /* protected void setupDrawer() {
-//        drawerToogle = new ActionBarDrawerToggle(this, relativeLayout,
-//                R.string.drawer_open, R.string.drawer_close) {
-//
-//            /** Called when a drawer has settled in a completely open state. */
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//                getSupportActionBar().setTitle("mActivityTitle!");
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//
-//            /** Called when a drawer has settled in a completely closed state. */
-//            public void onDrawerClosed(View view) {
-//                super.onDrawerClosed(view);
-//                getSupportActionBar().setTitle(activityTitle);
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//        };
-//
-//        //drawerToogle.setDrawerIndicatorEnabled(true);
-//        //relativeLayout.addDrawerListener(drawerToogle);
-//
-//        //drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                switch ((int) id) {
-//                    case 0: // Replace it by constants
-//                        return;
-//                    case 1:
-//                        return;
-//                    case 2:
-//                        return;
-//                    case 3:
-//                        return;
-//                    case 4:
-//                        return;
-//                    case 5:
-//                        return;
-//
-//                }
-//            }
-//        });*/
-//    }
+    protected void setupDrawer() {
+        drawerToogle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("mActivityTitle!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        drawerToogle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToogle);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch ((int) id) {
+                    case 0: // Replace it by constants
+                        return;
+                    case 1:
+                        return;
+                    case 2:
+                        return;
+                    case 3:
+                        return;
+                    case 4:
+                        return;
+                    case 5:
+                        return;
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        // Activate the navigation drawer toggle
+        if (drawerToogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
