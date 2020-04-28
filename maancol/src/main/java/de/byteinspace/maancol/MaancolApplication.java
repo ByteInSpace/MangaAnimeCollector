227b38644065ead41c42ee339942e0aeb4ccd269
Daniel Ludwig <contact@daniel-ludwig.net>
2020-04-28 13:00:27 +0200

    Initial setup for SpringBoot

---
+++ maancol/src/main/java/de/byteinspace/maancol/MaancolApplication.java
@@ -0,0 +1,13 @@
+package de.byteinspace.maancol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaancolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaancolApplication.class, args);
+	}
+
+}
