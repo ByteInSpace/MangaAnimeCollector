ea5d520b509a6e67c0bf540a5ec4ba658c895c6e
Daniel Ludwig <contact@daniel-ludwig.net>
2020-04-28 15:34:54 +0200

    Initial

---
+++ mangaanime/src/main/java/de/byteinspace/mangaanime/MangaanimeApplication.java
@@ -0,0 +1,13 @@
+package de.byteinspace.mangaanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaanimeApplication.class, args);
+	}
+
+}
---
+++ mangaanime/src/test/java/de/byteinspace/mangaanime/MangaanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.byteinspace.mangaanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
